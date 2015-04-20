6748db22e76ff4e14206f1780fd532e780b5e0bd
Karol Chodakowski <flataux@gmail.com>
2015-04-20 14:45:16 +0200

    Basic view now returns minimal HTML
    Next: http://chimera.labs.oreilly.com/books/1234000000754/ch04.html

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-    pass
+def home_page(request):
+    content = b'<html><head><title>To-Do lists</title></head><body></body></html>'
+    response = HttpResponse(content)
+    return response
